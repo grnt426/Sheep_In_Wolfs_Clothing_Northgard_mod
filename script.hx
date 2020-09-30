2ec25eb26914d9c9cd7042666167bacfa7524b71
Grant Kurtz <kurtz.grant@gmail.com>
2020-09-30 16:13:38 -0400

    Fully working game. Not balanced.

--- script.hx
+++ script.hx
@@ -6,7 +6,7 @@ var LORE_REWARD = {res:Resource.Lore, amt:60.0, mul:1.0, cb:"chooseLore", name:"
 var MONEY_REWARD = {res:Resource.Money, amt:50.0, mul:1.0, cb:"chooseMoney", name:"Money"};
 var WOOD_REWARD = {res:Resource.Wood, amt:75.0, mul:1.0, cb:"chooseWood", name:"Wood"};
 var FOOD_REWARD = {res:Resource.Food, amt:75.0, mul:1.0, cb:"chooseFood", name:"Food"};
-var FAME_REWARD = {res:Resource.Fame, amt:60.0, mul:1.0, cb:"chooseFame", name:"Fame"};
+var FAME_REWARD = {res:Resource.Fame, amt:30.0, mul:0.5, cb:"chooseFame", name:"Fame"};
 var STONE_REWARD = {res:Resource.Stone, amt:8.0, mul:0.75, cb:"chooseStone", name:"Stone"};
 var IRON_REWARD = {res:Resource.Iron, amt:5.0, mul:0.75, cb:"chooseIron", name:"Iron"};
 
@@ -43,8 +43,19 @@ var ME_ARGS : Array<Dynamic> = [];
 var SHEEP_OBJ_ID = "SHEEPSHEEP";
 var FAME_OBJ_ID = "FAMEFAME";
 var SHEEP_INDEX = 0;
+var AI_INDEX = 0;
 
-var playerData : Array<{p:Player, sheeps:Int, resChoices:Array<String>, unitChoices:Array<String>}> = [];
+var winningPlayer:Player = null;
+var winningTime = 0.0;
+var victoryMessage = "";
+var lossMessage = "";
+
+/**
+ * Incremented once per call to regularUpdate. Used for sending messages irregularly.
+ */
+var UPDATE_INDEX = 0;
+
+var playerData : Array<{p:Player, sheeps:Int, resChoices:Array<String>, unitChoices:Array<String>, isDead:Bool}> = [];
 var hostPlayer = null;
 
 function init() {
@@ -57,8 +68,6 @@ function onFirstLaunch() {
 		hostPlayer = me();
 
 		addRule(Rule.PillarOfGod);
-		addRule(Rule.PurchaseTechWithKrowns);
-		addRule(Rule.DefendedTownhalls);
 		addRule(Rule.ExtraFoodProduce);
 
 		state.removeVictory(VictoryKind.VLore);
@@ -67,11 +76,11 @@ function onFirstLaunch() {
 
 		for(p in state.players) {
 			p.discoverAll();
-			playerData.push({p:p, sheeps:0, resChoices:[], unitChoices:[]});
+			playerData.push({p:p, sheeps:0, resChoices:[], unitChoices:[], isDead:false});
+			p.objectives.add(FAME_OBJ_ID, "Reach 1500 Fame", {visible:true});
+			p.objectives.add(SHEEP_OBJ_ID, "Reach 100 Sheep", {showOtherPlayers:true, goalVal:100, showProgressBar:true, val:0, visible:true});
+			p.setAILevel(5); // will be ignored for non-AI players
 		}
-
-		state.objectives.add(FAME_OBJ_ID, "Reach 1500 Fame");
-		state.objectives.add(SHEEP_OBJ_ID, "Reach 100 Sheep", {showOtherPlayers:true, goalVal:100});
 
 		for(o in CHOOSE_RES) {
 			createButtons(o.name, o.cb, o.amt, o.mul);
@@ -109,42 +118,143 @@ function regularUpdate(dt : Float) {
 			checkNewChoices(),
 
 			checkSheeps(),
+
+			checkAI(),
+
+			checkTheDead(),
+
+			endGame(),
 		];
+	}
+
+	UPDATE_INDEX++;
+}
+
+function endGame() {
+	if(winningTime > 0 && winningTime + 10 < state.time) {
+		winningPlayer.customVictory(victoryMessage, lossMessage);
 	}
 }
 
-function checkSheeps() {
-	var data = playerData[SHEEP_INDEX];
-	data.sheeps = 0;
-	var player = data.p;
-
-	// TODO can wolf capture anything else on this tiny map? If not, then just getting
-	// the length of this will save a lot of time
-	for(u in player.capturedUnits) {
-		if(u.kind == Unit.Sheep) {
-			data.sheeps++;
+function findResource(id:String) {
+	for(r in CHOOSE_RES) {
+		if(r.cb == id){
+			return r;
 		}
 	}
-	SHEEP_INDEX = (SHEEP_INDEX + 1) % 8;
+
+	debug("NOT FOUND RESOURCE!");
+	return null;
+}
+
+function findUnit(id:String) {
+	for(u in CHOOSE_UNIT) {
+		if(u.cb == id){
+			return u;
+		}
+	}
+
+	debug("NOT FOUND UNIT!");
+	return null;
+}
+
+function checkTheDead() {
+	@sync for(d in playerData) {
+		if(d.isDead)
+			continue;
+		var found = false;
+		for(p in state.players) {
+			if(d.p == p)
+				found = true;
+		}
+		if(!found) {
+			debug(d.p + " Is dead!");
+			d.isDead = true;
+		}
+	}
+}
+
+function checkAI() {
+
+	var data = playerData[AI_INDEX];
+	if(data.p.isAI) {
+		if(data.resChoices.length > 0) {
+			var choice = data.resChoices[randomInt(3)];
+			giveResourceReward(data.p, findResource(choice));
+		}
+		if(data.unitChoices.length > 0) {
+			var choice = data.unitChoices[randomInt(3)];
+			giveUnitReward(data.p, findUnit(choice));
+		}
+	}
+
+	AI_INDEX = (AI_INDEX + 1) % 8;
+}
+
+function checkSheeps() {
+	for(d in playerData) {
+		if(d.isDead)
+			continue;
+		d.sheeps = d.p.capturedUnits.length;
+	}
 }
 
 /**
  * [Host Only]
  */
 function checkVictory() {
+	if(winningPlayer != null)
+		return;
+
 	for(d in playerData) {
+		if(d.isDead)
+			continue;
 		var fame = d.p.getResource(Resource.Fame);
 		var sheeps = d.sheeps;
 		if(fame >= 1500) {
-			d.p.customVictory("You are a very shiny and famous sheep", "You were not shiny enough");
+			winningPlayer = d.p;
+			winningTime = state.time;
+			victoryMessage = "You are a very shiny and famous sheep";
+			lossMessage = "You were not shiny enough";
+			d.p.objectives.setStatus(FAME_OBJ_ID, OStatus.Done);
+			d.p.genericNotify("You have won a fame victory! The game will end shortly. TAUNT YOUR ENEMIES! :D");
+			for(p in state.players)
+				if(p == d.p)
+					continue;
+				else {
+					p.objectives.setStatus(FAME_OBJ_ID, OStatus.Missed);
+					p.genericNotify("You have failed to achieve a fame victory! The game will end shortly.");
+				}
+			break;
 		} else if(sheeps >= 100) {
-			d.p.customVictory("You are a very fluffy sheep", "You were not fluffy enough");
+			winningPlayer = d.p;
+			winningTime = state.time;
+			victoryMessage = "You are a very fluffy sheep";
+			lossMessage = "You were not fluffy enough";
+			d.p.objectives.setStatus(SHEEP_OBJ_ID, OStatus.Done);
+			d.p.genericNotify("YOU LOVE THE SHEEPS! CONGRATS! You are the SHEEPIEST! The game will end shortly.");
+			for(p in state.players)
+				if(p == d.p)
+					continue;
+				else {
+					p.objectives.setStatus(SHEEP_OBJ_ID, OStatus.Missed);
+					p.genericNotify("YOU NO LIKE SHEEP?! TOO BAD YOU LOSE! The game will end shortly.");
+				}
+			break;
 		}
+	}
 
-		if(d.p != hostPlayer)
-			state.objectives.setOtherPlayerVal(SHEEP_OBJ_ID, d.p, sheeps);
-		else
-			state.objectives.setCurrentVal(SHEEP_OBJ_ID, sheeps);
+	@sync for(d in playerData) {
+		if(d.isDead)
+			continue;
+		for(other in state.players) {
+			if(other == d.p) {
+				d.p.objectives.setCurrentVal(SHEEP_OBJ_ID, d.sheeps);
+			}
+			else {
+				other.objectives.setOtherPlayerVal(SHEEP_OBJ_ID, d.p, d.sheeps);
+			}
+		}
 	}
 }
 
@@ -153,14 +263,14 @@ function checkVictory() {
  */
 function checkNewChoices() {
 	if(state.time / 30 > CHOICE_INDEX) {
-		msg("Clearing choices and setting up the next round");
 		CHOICE_INDEX++;
 
 		// decide and show new choices
 		@sync for(c in playerData) {
-			var player = c.p;
-			if(player.isAI)
+			if(c.isDead)
 				continue;
+
+			var player = c.p;
 
 			var oldResChoices = c.resChoices.copy();
 			c.resChoices = [];
@@ -172,9 +282,11 @@ function checkNewChoices() {
 			for(i in 0...3) {
 				var choice = resChoices.pop().cb;
 				c.resChoices.push(choice);
-				player.objectives.setVisible(choice, true);
-				if(oldResChoices.indexOf(choice) != -1)
-					oldResChoices.remove(choice);
+				if(!player.isAI){
+					player.objectives.setVisible(choice, true);
+					if(oldResChoices.indexOf(choice) != -1)
+						oldResChoices.remove(choice);
+				}
 			}
 
 			var unitChoices = CHOOSE_UNIT.copy();
@@ -182,17 +294,21 @@ function checkNewChoices() {
 			for(i in 0...3) {
 				var choice = unitChoices.pop().cb;
 				c.unitChoices.push(choice);
-				player.objectives.setVisible(choice, true);
-				if(oldUnitChoices.indexOf(choice) != -1) {
-					oldUnitChoices.remove(choice);
+				if(!player.isAI){
+					player.objectives.setVisible(choice, true);
+					if(oldUnitChoices.indexOf(choice) != -1) {
+						oldUnitChoices.remove(choice);
+					}
 				}
 			}
 
-			for(c in oldResChoices){
-				player.objectives.setVisible(c, false);
-			}
-			for(c in oldUnitChoices){
-				player.objectives.setVisible(c, false);
+			if(!player.isAI) {
+				for(c in oldResChoices){
+					player.objectives.setVisible(c, false);
+				}
+				for(c in oldUnitChoices){
+					player.objectives.setVisible(c, false);
+				}
 			}
 		}
 	}
@@ -202,14 +318,12 @@ function checkNewChoices() {
  * [Host Only]
  */
 function clearChoices(p:Player, choices:Array<String>) {
-	debug("Clearing choices");
 	for(c in choices) {
 		p.objectives.setVisible(c, false);
-		debug("Cleared: " + c);
 	}
 }
 
-function getplayerData(p:Player): {p:Player, resChoices:Array<String>, unitChoices:Array<String>} {
+function getplayerData(p:Player): {p:Player, resChoices:Array<String>, unitChoices:Array<String>, isDead:Bool} {
 	for(c in playerData) {
 		if(c.p == p) {
 			return c;
@@ -224,7 +338,6 @@ function getplayerData(p:Player): {p:Player, resChoices:Array<String>, unitChoic
  * Returns a whole number of years that have passed.
  */
 function timeToYears(time:Float):Int {
-	debug("COMPUTING YEARS, I GUESS");
 	return toInt(time / 720.0) + 1;
 }
 
@@ -236,10 +349,10 @@ function computeTotalReward(amt:Float, mul:Float):Int {
  * [Host Only]
  */
 function giveResourceReward(p:Player, res:{res:ResourceKind, amt:Float, mul:Float, cb:String, name:String}) {
-	msg("Giving a player a resource reward");
 	p.addResource(res.res, computeTotalReward(res.amt, res.mul));
 	var data = getplayerData(p);
-	clearChoices(p, data.resChoices);
+	if(!p.isAI)
+		clearChoices(p, data.resChoices);
 	data.resChoices = [];
 }
 
@@ -247,10 +360,10 @@ function giveResourceReward(p:Player, res:{res:ResourceKind, amt:Float, mul:Floa
  * [Host Only]
  */
 function giveUnitReward(p:Player, unit:{type:UnitKind, amt:Float, mul:Float, cb:String, name:String}) {
-	msg("Giving a player unit reward");
 	var units = p.getTownHall().zone.addUnit(unit.type, computeTotalReward(unit.amt, unit.mul), p, true);
 	var data = getplayerData(p);
-	clearChoices(p, data.unitChoices);
+	if(!p.isAI)
+		clearChoices(p, data.unitChoices);
 	data.unitChoices = [];
 }
 
@@ -261,63 +374,31 @@ function hostVer(str:String) {
 // =================== Resource Selection ======================
 
 function chooseIron() {
-	if(me() == hostPlayer){
-		chooseIron_host(me());
-	}
-	else
-		invokeHost(hostVer(IRON_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(IRON_REWARD.cb), ME_ARGS);
 }
 
 function chooseStone() {
-	if(me() == hostPlayer){
-		chooseStone_host(me());
-	}
-	else
-		invokeHost(hostVer(STONE_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(STONE_REWARD.cb), ME_ARGS);
 }
 
 function chooseFame() {
-	if(me() == hostPlayer){
-		chooseFame_host(me());
-	}
-	else
-		invokeHost(hostVer(FAME_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(FAME_REWARD.cb), ME_ARGS);
 }
 
 function chooseFood() {
-	debug("FOOD CLICKED");
-	if(me() == hostPlayer){
-		chooseFood_host(me());
-	}
-	else{
-		invokeHost(hostVer(FOOD_REWARD.cb), ME_ARGS);
-	}
+	invokeHost(hostVer(FOOD_REWARD.cb), ME_ARGS);
 }
 
 function chooseWood() {
-	debug("WOOD CLICKED");
-	if(me() == hostPlayer){
-		chooseWood_host(me());
-	}
-	else
-		invokeHost(hostVer(WOOD_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(WOOD_REWARD.cb), ME_ARGS);
 }
 
 function chooseMoney() {
-	debug("MONEY CLICKED");
-	if(me() == hostPlayer){
-		chooseMoney_host(me());
-	}
-	else
-		invokeHost(hostVer(MONEY_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(MONEY_REWARD.cb), ME_ARGS);
 }
 
 function chooseLore() {
-	if(me() == hostPlayer){
-		chooseLore_host(me());
-	}
-	else
-		invokeHost(hostVer(LORE_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(LORE_REWARD.cb), ME_ARGS);
 }
 
 function chooseIron_host(p:Player) {
@@ -351,44 +432,23 @@ function chooseLore_host(p:Player) {
 // =================== Unit Selection ======================
 
 function chooseWarrior() {
-	if(me() == hostPlayer){
-		chooseWarrior_host(me());
-	}
-	else
-		invokeHost(hostVer(WARRIOR_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(WARRIOR_REWARD.cb), ME_ARGS);
 }
 
 function chooseSheep() {
-	debug("SHEEP CLICKED");
-	if(me() == hostPlayer){
-		chooseSheep_host(me());
-	}
-	else
-		invokeHost(hostVer(SHEEP_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(SHEEP_REWARD.cb), ME_ARGS);
 }
 
 function chooseShieldBearer() {
-	if(me() == hostPlayer){
-		chooseShieldBearer_host(me());
-	}
-	else
-		invokeHost(hostVer(SHIELD_BEARER_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(SHIELD_BEARER_REWARD.cb), ME_ARGS);
 }
 
 function chooseAxeWielder() {
-	if(me() == hostPlayer){
-		chooseAxeWielder_host(me());
-	}
-	else
-		invokeHost(hostVer(AXE_WIELDER_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(AXE_WIELDER_REWARD.cb), ME_ARGS);
 }
 
 function chooseVillager() {
-	if(me() == hostPlayer){
-		chooseVillager_host(me());
-	}
-	else
-		invokeHost(hostVer(VILLAGER_REWARD.cb), ME_ARGS);
+	invokeHost(hostVer(VILLAGER_REWARD.cb), ME_ARGS);
 }
 
 function chooseWarrior_host(p:Player) {
